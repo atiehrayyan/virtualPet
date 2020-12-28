a11630fd52958f6d37fbe4377c51a14f3ef0a5fc
atiehrayyan <71940602+atiehrayyan@users.noreply.github.com>
2020-12-28 10:46:27 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+//Create variables here
+var dog , happyDog;
+var database;
+var foodS , foodStock
+
+function preload(){
+  //load images here
+  dog = loadImage("images/dogImg.png");
+}
+
+function setup() {
+	createCanvas(500, 500);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
